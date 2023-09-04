328a253267298c21fc06247247e0e22d56b20b45
mehmetJava8Hibernate <mehmetarif96@gmail.com>
2023-09-04 20:58:58 +0300

    user-service metodlari yazildi task-41

---
+++ user-micro-service/src/main/java/com/hrmanagement/exceptions/UserManagerException.java
@@ -0,0 +1,15 @@
+package com.hrmanagement.exceptions;
+
+import lombok.Getter;
+
+@Getter
+public class UserManagerException extends RuntimeException{
+    private final ErrorType errorType;
+    public UserManagerException(ErrorType errorType) {
+        this.errorType=errorType;
+    }
+    public UserManagerException(ErrorType errorType, String customMessage) {
+        super(customMessage);
+        this.errorType= errorType;
+    }
+}
